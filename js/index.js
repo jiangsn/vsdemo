ad987ef6adc673626fc0287dee3cd38572dc42b1
jiangxue <994898372@qq.com>
2018-10-16 00:29:31 +0800

    Add Animated bootstrap jquery.

---
+++ js/index.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $("button").addClass("btn btn-primary");
+    $("button").addClass("animated shake");
+    $("button").addClass('animated bounce');
+    $("button").addClass('animated fadeOut');
+    $("body").addClass("animated hinge");
+});
